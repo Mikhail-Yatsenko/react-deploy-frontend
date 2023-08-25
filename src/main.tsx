076c61440d5ee3f333628f24b542ed30c76ea1e1
yacenko <mikhail.yacenko@fabit.ru>
2023-08-25 12:44:13 +0300

    added routing and some pages

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,29 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import * as React from "react";
+import * as ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import {About} from "./pages/About.tsx";
+import {Welcome} from "./pages/Welcome.tsx";
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <App />,
+        children: [
+            {
+                path: 'about',
+                element: <About />,
+            },
+            {
+                path: 'Welcome',
+                element: <Welcome />,
+            }
+        ]
+    }
+])
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <React.StrictMode>
+        <RouterProvider router={router} />
+    </React.StrictMode>
 )
---
+++ src/pages/Welcome.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+
+export const Welcome = () => {
+    return (
+        <div>
+            Welcome page
+        </div>
+    );
+};
